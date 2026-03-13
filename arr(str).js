feb5982b6ed3ac0c82baed598f65a98b780d7d35
BinduReddy1610 <hima0714u@gmail.com>
2026-03-13 11:29:33 +0530

    Update arr(str).js

--- arr(str).js
+++ arr(str).js
@@ -1,4 +1,4 @@
-let names=['Bindu','Manasa','Janu','Indu','Prashu'];
-console.log(Name[0]);
+let Names=['Bindu','Manasa','Janu','Indu','Prashu'];
+console.log(Names[0]);
 console.log(Names[3]);
-console.log(Name[4]);
+console.log(Names[4]);
